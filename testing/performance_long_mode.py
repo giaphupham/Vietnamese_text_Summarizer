a67b0f2628d02b4aa91650feb8c05825172a5b91
tindaking <82101177+TinDaKing@users.noreply.github.com>
2024-05-02 21:56:03 +0700

    add data for performance test

--- testing/performance_long_mode.py
+++ testing/performance_long_mode.py
@@ -1,5 +1,5 @@
-from locust import SequentialTaskSet, HttpUser, constant, task # locust 2.26.0
-import json, logging
+from locust import SequentialTaskSet, HttpUser, constant, task, between # locust 2.26.0
+import json, logging, sys
 
 
 class LongSummarizer(SequentialTaskSet):
@@ -16,7 +16,6 @@ class LongSummarizer(SequentialTaskSet):
             'Accept-Encoding': 'gzip, deflate, br, zstd',
             'Accept-Language': 'en-US,en;q=0.9',
             'Connection': 'keep-alive',
-            'Content-Length': '1118',
             'Content-Type': 'application/json',
             'Cookie': 'session=qW7Gqkdq9t4A3sz6zS94_ptTNKf8azc5Tp5bjh9NW8c',
             'Host': '127.0.0.1:5000', # replace this with hosted backend server
@@ -30,7 +29,7 @@ class LongSummarizer(SequentialTaskSet):
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
         }   
         with self.client.post("/login", data=json.dumps(data), headers=headers, catch_response=True) as response:
-            if response.status_code == 403 or response.status_code == 200: # 403 is when reditected to home but denied because cannot access to session.. idk why it cannnot access to session at /home api
+            if response.status_code == 403 or response.status_code == 200: # 403 is when reditected to home but denied because cannot access to session.. idk why it cannnot access to session at /home
                 response.success()
                 logging.error("Login successfully!")
             else:
@@ -42,19 +41,12 @@ class LongSummarizer(SequentialTaskSet):
             'Accept': 'application/json, text/plain, */*',
             'Accept-Encoding': 'gzip, deflate, br, zstd',
             'Accept-Language': 'en-US,en;q=0.9',
-            'Connection': 'keep-alive',
-            'Content-Length': '1118',
-            'Content-Type': 'application/json',
+            'Connection': 'close',
             'Cookie': 'session=qW7Gqkdq9t4A3sz6zS94_ptTNKf8azc5Tp5bjh9NW8c',
             'Host': '127.0.0.1:5000', # replace this with hosted backend server
             'Origin': 'https://vietnamese-text-summarizer.onrender.com',
-            'Sec-Ch-Ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
-            'Sec-Ch-Ua-Mobile': '?0',
-            'Sec-Ch-Ua-Platform': '"Windows"',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'cross-site',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+        
         }
         response = self.client.get("/home", headers=headers)    
 
@@ -65,9 +57,21 @@ class LongSummarizer(SequentialTaskSet):
 
     @task
     def summarize_long_mode(self):
+
+        def read_file(file_path):
+            try:
+                with open(file_path, 'r', encoding='utf-8') as file:
+                    content = file.read()
+                    return content
+            except FileNotFoundError:
+                return "Error: File '{}' not found.".format(file_path)
+            except Exception as e:
+                return "Error: " +str(e)
+        text = read_file('data/around500words.txt')
+        # print(text)
         data ={
-            'input-text': 'Tổng giám đốc Apple Tim Cook đã có mặt tại Hà Nội rạng sáng nay, dự kiến gặp gỡ một số nhà sáng tạo nội dung và lập trình viên Việt.Không có nơi nào như Việt Nam, một đất nước sôi động và xinh đẹp. Tôi vô cùng hào hứng khi được tới đây kết nối với sinh viên, nhà sáng tạo và khách hàng, hiểu hơn về sự đa dạng trong cách họ sử dụng sản phẩm của chúng tôi để làm nên những điều phi thường, Tim Cook chia sẻ ngay khi vừa đến Hà Nội. Trong chuyến thăm lần này của CEO, Apple cũng thông báo tăng cường cam kết với Việt Nam. Công ty cho biết sẽ nâng các khoản chi cho nhà cung cấp, cùng với tiến triển mới trong sáng kiến hỗ trợ nước sạch cho trường học địa phương. Apple đã chi gần 400 nghìn tỷ đồng kể từ năm 2019 thông qua chuỗi cung ứng địa phương và đã tăng hơn gấp đôi mức chi hàng năm cho Việt Nam trong cùng kỳ.',
-            'sentences': 6,
+            'input-text': text,
+            'sentences': 23,
             'withCredentials':'true'
         }
         headers = {
@@ -75,7 +79,7 @@ class LongSummarizer(SequentialTaskSet):
             'Accept-Encoding': 'gzip, deflate, br, zstd',
             'Accept-Language': 'en-US,en;q=0.9',
             'Connection': 'keep-alive',
-            'Content-Length': '1118',
+            'Content-Length': '3507',
             'Content-Type': 'application/json',
             'Cookie': 'session=qW7Gqkdq9t4A3sz6zS94_ptTNKf8azc5Tp5bjh9NW8c',
             'Host': '127.0.0.1:5000', # replace this with hosted backend server
@@ -88,13 +92,19 @@ class LongSummarizer(SequentialTaskSet):
             'Sec-Fetch-Site': 'cross-site',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
         }
-        response = self.client.post("/summarize-long", data=json.dumps(data), headers=headers)  
+        
+        response = self.client.post("/summarize-long", data=json.dumps(data), headers=headers) 
+
+        data = response.json()
+        # print(data.get('output-text'))
         if response.status_code == 200:
             logging.info("Summarized long mode successfully!")
         else:
             logging.error("Failed to summarize long mode. Status code: %d" % response.status_code)  
 
+    
+
 class MyLoadTest(HttpUser):
     host = "http://127.0.0.1:5000"
     tasks = [LongSummarizer]
-    wait_time = constant(2)
+    wait_time = between(0.1, 0.5)
---
+++ testing/performance_short_mode.py
@@ -0,0 +1,110 @@
+from locust import SequentialTaskSet, HttpUser, constant, task, between # locust 2.26.0
+import json, logging, sys
+
+
+class ShortSummarizer(SequentialTaskSet):
+
+    @task
+    def login(self):
+        data ={
+            'username':'nguyenphamkien15@gmail.com',
+            'password':'123',
+            'withCredentials':'true'
+        }
+        headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/json',
+            'Cookie': 'session=qW7Gqkdq9t4A3sz6zS94_ptTNKf8azc5Tp5bjh9NW8c',
+            'Host': '127.0.0.1:5000', # replace this with hosted backend server
+            'Origin': 'https://vietnamese-text-summarizer.onrender.com',
+            'Sec-Ch-Ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+            'Sec-Ch-Ua-Mobile': '?0',
+            'Sec-Ch-Ua-Platform': '"Windows"',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'cross-site',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+        }   
+        with self.client.post("/login", data=json.dumps(data), headers=headers, catch_response=True) as response:
+            if response.status_code == 403 or response.status_code == 200: # 403 is when reditected to home but denied because cannot access to session.. idk why it cannnot access to session at /home
+                response.success()
+                logging.error("Login successfully!")
+            else:
+                logging.error("Failed to log in. Status code: %d" % response.status_code)
+    
+    @task
+    def home(self):
+        headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Connection': 'close',
+            'Cookie': 'session=qW7Gqkdq9t4A3sz6zS94_ptTNKf8azc5Tp5bjh9NW8c',
+            'Host': '127.0.0.1:5000', # replace this with hosted backend server
+            'Origin': 'https://vietnamese-text-summarizer.onrender.com',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+        
+        }
+        response = self.client.get("/home", headers=headers)    
+
+        if response.status_code == 200: 
+            logging.info("At home")
+        else:
+            logging.error("Failed access home. Status code: %d" % response.status_code)
+
+    @task
+    def summarize_short_mode(self):
+
+        def read_file(file_path):
+            try:
+                with open(file_path, 'r', encoding='utf-8') as file:
+                    content = file.read()
+                    return content
+            except FileNotFoundError:
+                return "Error: File '{}' not found.".format(file_path)
+            except Exception as e:
+                return "Error: " +str(e)
+        text = read_file('data/around500words.txt')
+        # print(text)
+        data ={
+            'input-text': text,
+            'sentences': 23,
+            'withCredentials':'true'
+        }
+        headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Connection': 'keep-alive',
+            'Content-Length': '3507',
+            'Content-Type': 'application/json',
+            'Cookie': 'session=qW7Gqkdq9t4A3sz6zS94_ptTNKf8azc5Tp5bjh9NW8c',
+            'Host': '127.0.0.1:5000', # replace this with hosted backend server
+            'Origin': 'https://vietnamese-text-summarizer.onrender.com',
+            'Sec-Ch-Ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+            'Sec-Ch-Ua-Mobile': '?0',
+            'Sec-Ch-Ua-Platform': '"Windows"',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'cross-site',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+        }
+        
+        response = self.client.post("/summarize-short", data=json.dumps(data), headers=headers) 
+
+        data = response.json()
+        # print(data.get('output-text'))
+        if response.status_code == 200:
+            logging.info("Summarized short mode successfully!")
+        else:
+            logging.error("Failed to summarize short mode. Status code: %d" % response.status_code)  
+
+    
+
+class MyLoadTest(HttpUser):
+    host = "http://127.0.0.1:5000"
+    tasks = [ShortSummarizer]
+    wait_time = between(0.1, 0.5)
