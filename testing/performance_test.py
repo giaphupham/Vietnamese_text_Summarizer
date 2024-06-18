c838c5dba62a75419f47eeddbb730ed1b49aa04f
tindaking <82101177+TinDaKing@users.noreply.github.com>
2024-06-18 16:15:54 +0700

    performance test update

--- testing/performance_test.py
+++ testing/performance_test.py
@@ -15,18 +15,19 @@ class Summarizer(SequentialTaskSet):
             'Accept': 'application/json, text/plain, */*',
             'Accept-Encoding': 'gzip, deflate, br, zstd',
             'Accept-Language': 'en-US,en;q=0.9',
+            'Access-Control-Request-Headers': 'content-type',
+            'Access-Control-Request-Method': 'POST',
             'Connection': 'keep-alive',
+            'Content-Length': '84',
             'Content-Type': 'application/json',
-            'Cookie': 'session=qW7Gqkdq9t4A3sz6zS94_ptTNKf8azc5Tp5bjh9NW8c',
-            'Host': '127.0.0.1:5000', # replace this with hosted backend server
-            'Origin': 'http://localhost:5173',
-            'Sec-Ch-Ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
-            'Sec-Ch-Ua-Mobile': '?0',
-            'Sec-Ch-Ua-Platform': '"Windows"',
+            'Cookie': 'session=trlD-HVJD65lUIOMLArFnEyUUluj66GSJ9vK6Y7Zb8U',
+            'Host': 'vietnamesetextsummarizer.azurewebsites.net',
+            'Origin': 'https://hcmusummarizer.azurewebsites.net',
+            'Referer': 'https://hcmusummarizer.azurewebsites.net/',
             'Sec-Fetch-Dest': 'empty',
             'Sec-Fetch-Mode': 'cors',
             'Sec-Fetch-Site': 'cross-site',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
         }   
         with self.client.post("/login", data=json.dumps(data), headers=headers, catch_response=True) as response:
             if response.status_code == 403 or response.status_code == 200: # 403 is when reditected to home but denied because cannot access to session.. idk why it cannnot access to session at /home
@@ -41,11 +42,19 @@ class Summarizer(SequentialTaskSet):
             'Accept': 'application/json, text/plain, */*',
             'Accept-Encoding': 'gzip, deflate, br, zstd',
             'Accept-Language': 'en-US,en;q=0.9',
-            'Connection': 'close',
-            'Cookie': 'session=qW7Gqkdq9t4A3sz6zS94_ptTNKf8azc5Tp5bjh9NW8c',
-            'Host': '127.0.0.1:5000', # replace this with hosted backend server
-            'Origin': 'http://localhost:5173',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/json',
+            'Cookie': 'session=trlD-HVJD65lUIOMLArFnEyUUluj66GSJ9vK6Y7Zb8U',
+            'Host': 'vietnamesetextsummarizer.azurewebsites.net',
+            'Origin': 'https://hcmusummarizer.azurewebsites.net',
+            'Referer': 'https://hcmusummarizer.azurewebsites.net/',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'cross-site',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"'
         
         }
         response = self.client.get("/home", headers=headers)    
@@ -71,7 +80,7 @@ class Summarizer(SequentialTaskSet):
         # print(text)
         data ={
             'input-text': text,
-            'sentences': 4,
+            'sentences': 0,
             'withCredentials':'true'
         }
         headers = {
@@ -79,18 +88,19 @@ class Summarizer(SequentialTaskSet):
             'Accept-Encoding': 'gzip, deflate, br, zstd',
             'Accept-Language': 'en-US,en;q=0.9',
             'Connection': 'keep-alive',
-            'Content-Length': '3507',
+            'Content-Length': '3216',
             'Content-Type': 'application/json',
-            'Cookie': 'session=qW7Gqkdq9t4A3sz6zS94_ptTNKf8azc5Tp5bjh9NW8c',
-            'Host': '127.0.0.1:5000', # replace this with hosted backend server
-            'Origin': 'https://localhost:5173',
-            'Sec-Ch-Ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
-            'Sec-Ch-Ua-Mobile': '?0',
-            'Sec-Ch-Ua-Platform': '"Windows"',
+            'Cookie': 'session=trlD-HVJD65lUIOMLArFnEyUUluj66GSJ9vK6Y7Zb8U',
+            'Host': 'vietnamesetextsummarizer.azurewebsites.net',
+            'Origin': 'https://hcmusummarizer.azurewebsites.net',
+            'Referer': 'https://hcmusummarizer.azurewebsites.net/',
             'Sec-Fetch-Dest': 'empty',
             'Sec-Fetch-Mode': 'cors',
             'Sec-Fetch-Site': 'cross-site',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"'
         }
         
         response = self.client.post("/summarize", data=json.dumps(data), headers=headers) 
@@ -98,9 +108,9 @@ class Summarizer(SequentialTaskSet):
         output_data = response.json()
         print(output_data.get('output-text'))
         if response.status_code == 200:
-            logging.info("Summarized long mode successfully!")
+            logging.info("Summarized successfully!")
         else:
-            logging.error("Failed to summarize long mode. Status code: %d" % response.status_code)  
+            logging.error("Failed to summarize. Status code: %d" % response.status_code)  
 
     
 
